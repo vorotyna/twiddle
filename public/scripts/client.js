e2b77906a0883f30a660633e6d31a96e0268f4fd
Alisa Vorotyntseva <alisa.vorotyn@gmail.com>
2022-11-10 02:43:42 +0000

    Fetch tweets with AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,3 @@
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 // Function turns tweet object into HTML tweet article
 const createTweetElement = function(data) {
   let newTweet = `
@@ -62,6 +35,7 @@ const createTweetElement = function(data) {
 
 // Function appends array of tweet objects into tweet article
 const renderTweets = function(data) {
+  $('#tweets-container').empty();
   for (let tweet of data) {
     const newTweetElement = createTweetElement(tweet);
     $('#tweets-container').append(newTweetElement);
@@ -73,21 +47,35 @@ $(document).ready(function() {
   console.log("The document is ready!");
 
   $("#submit-tweet").on("submit", function(event) {
-    console.log("Tweet submitted");
     event.preventDefault();
-
     $.ajax({
       method: "POST",
       url: "/tweets",
       data: $("#submit-tweet").serialize(),
     })
-      .then(function(result) {
-        console.log("Tweet has been sent to the server");
-        $("#tweet-text").val('');
+      .then(function(newResult) {
+        loadTweets();
       })
       .catch(function(error) {
         console.log("error", error);
       });
   });
-  renderTweets(data);
+  const loadTweets = function() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+    })
+      .then(function(newResult) {
+        $("#tweet-text").html(renderTweets(newResult));
+      })
+      .catch(function(error) {
+        console.log("error", error);
+      });
+  };
+
+  loadTweets();
 });
+
+
+
+
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const userHelper    = require("../lib/util/user-helper")
+const userHelper = require("../lib/util/user-helper");
 
-const express       = require('express');
-const tweetsRoutes  = express.Router();
+const express = require('express');
+const tweetsRoutes = express.Router();
 
 module.exports = function(DataHelpers) {
 
@@ -19,7 +19,7 @@ module.exports = function(DataHelpers) {
 
   tweetsRoutes.post("/", function(req, res) {
     if (!req.body.text) {
-      res.status(400).json({ error: 'invalid request: no data in POST body'});
+      res.status(400).json({ error: 'invalid request: no data in POST body' });
       return;
     }
 
@@ -31,7 +31,7 @@ module.exports = function(DataHelpers) {
       },
       created_at: Date.now()
     };
-
+    console.log("Tweet:", tweet);
     DataHelpers.saveTweet(tweet, (err) => {
       if (err) {
         res.status(500).json({ error: err.message });
@@ -43,4 +43,4 @@ module.exports = function(DataHelpers) {
 
   return tweetsRoutes;
 
-}
+};
